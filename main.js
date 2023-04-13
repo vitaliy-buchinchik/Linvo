e797097a3500233fc691ae6db506148ed25ffda4
vitaliy-buchinchik <v.buchinchik@gmail.com>
2023-04-13 12:13:37 +0300

    Fixed main.js file

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-// const hamburger = document.getElementById("hamburger");
-// const popup = document.getElementById("popup");
-// const menu = document.getElementById("menu");
-// const body = document.querySelector("body");
-
-// function hamburgerHandler() {
-//   popup.classList.toggle("popup_open");
-//   hamburger.classList.toggle("hamburger_open");
-//   body.classList.toggle("mobile_body");
-//   popup.appendChild(menu);
-// }
-
-// hamburger.addEventListener("click", hamburgerHandler);
-
 const menu = document.querySelector('.mobile_nav');
 const menuBtn = document.querySelector('.menu_icon');
 
